395c9f8354f28a284d7dfbb888794d1610dd8c03
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2018-09-18 17:34:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 module.exports = function solveEquation(equation) {
-let arrOfNumbers = equation.split(/ /);
+  let arrOfNumbers = equation.split(/ /);
   const a = arrOfNumbers[0];
   const b = arrOfNumbers[3]+arrOfNumbers[4];
   const c = arrOfNumbers[7]+arrOfNumbers[8];
   
   let solutions = [];
   let d=b*b-4*a*c;
-  let sol1=Math.round((-b-Math.sqrt(d))/2/a);
-  let sol2=Math.round((-b+Math.sqrt(d))/2/a);
-  if(sol1>sol2) {
-    solutions. push(sol2);
-    solutions. push(sol1);
-  }
-  else {
-    solutions. push(sol1);
-    solutions. push(sol2);
-  }
+  solutions.push(Math.round((-b-Math.sqrt(d))/2/a));
+  solutions.push(Math.round((-b+Math.sqrt(d))/2/a));
+  solutions.sort((a, b) => a-b);
   return solutions;
 }
